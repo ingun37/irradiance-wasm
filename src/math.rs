617cf001f8ab9f2f1898fb3c9d73060aa225b70d
Ingun Jon <ingun37@gmail.com>
2022-01-30 10:44:25 +0900

    hemisphere

--- src/math.rs
+++ src/math.rs
@@ -6,7 +6,7 @@ pub fn fibonacci_hemi_sphere(sample_size: u32) -> Vec<f32> {
     let sample_size_f = sample_size as f32;
     for i in 0..sample_size {
         let i_f = i as f32;
-        let y = 1f32 - (i_f / (sample_size_f - 1f32)) * 2f32; // y goes from 1 to -1
+        let y = 1f32 - (i_f / (sample_size_f - 1f32)); // y goes from 1 to 0 (hemisphere)
         let radius = (1f32 - y * y).sqrt(); //  # radius at y
         let theta = phi * i_f; //  # golden angle increment
         let x = theta.cos() * radius;

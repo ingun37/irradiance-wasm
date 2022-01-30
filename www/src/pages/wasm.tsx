102ad1720a391a5d0044905b3ca3862ff246aca9
Ingun Jon <ingun37@gmail.com>
2022-01-30 10:24:05 +0900

    three render

--- www/src/pages/wasm.tsx
+++ www/src/pages/wasm.tsx
@@ -1,13 +1,37 @@
-import * as React from "react"
+import * as React from "react";
 import * as wasm from "irradiance-wasm";
-import {useEffect} from "react";
+import { useEffect } from "react";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
 
-const Wasm = ()=>{
-    useEffect(()=>{
-        console.log("wasm starting")
-        const arr = wasm.fibonacci_hemi_sphere(10);
-        console.log(arr);
-    })
-    return (<div>this is wasm page</div>)
-}
+const Wasm = () => {
+  useEffect(() => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new WebGLRenderer();
+    renderer.setSize(512, 512);
+    document.getElementById("wasmDiv").appendChild(renderer.domElement);
+
+    const geometry = new BoxGeometry();
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+    requestAnimationFrame(() => renderer.render(scene, camera));
+    const arr = wasm.fibonacci_hemi_sphere(10);
+  });
+  return <div id="wasmDiv" style={{ height: "512px", width: "512px" }} />;
+};
 export default Wasm;

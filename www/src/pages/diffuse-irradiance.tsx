8a092eab1374ed34062b4f950b04caf9a9bb6d77
Ingun Jon <ingun37@gmail.com>
2022-02-04 00:44:31 +0900

    tidy ui

---
+++ www/src/pages/diffuse-irradiance.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import * as wasm from "../../../pkg";
+import * as wasm_bg from "../../../pkg/irradiance_wasm_bg.wasm";
+import { useEffect, useState } from "react";
+import { interval, Subject, zip } from "rxjs";
+import { downloadBlob } from "../util";
+const DiffuseIrradiance = () => {
+  const [rx] = useState(new Subject<Uint8Array>());
+  const onclick = () => {
+    fetch("./venetian_crossroads_1k.hdr")
+      .then((x) => x.arrayBuffer())
+      .then((x) => new Uint8Array(x))
+      .then((ab) => {
+        wasm.irradiance(
+          1000,
+          64,
+          ab,
+          (idx: bigint, offset: number, size: bigint) => {
+            const hdrBuf = new Uint8Array(
+              wasm_bg.memory.buffer,
+              offset,
+              Number(size)
+            );
+            const cp = new Uint8Array(new ArrayBuffer(Number(size)));
+            cp.set(hdrBuf);
+            rx.next(cp);
+          }
+        );
+      });
+  };
+  useEffect(() => {
+    zip(interval(2000), rx).subscribe({
+      next([idx, buf]) {
+        downloadBlob(buf, idx.toString() + ".hdr");
+      },
+    });
+  }, []);
+  return <button onClick={onclick}>click me</button>;
+};
+export default DiffuseIrradiance;
--- www/src/pages/wasm.tsx
+++ www/src/pages/wasm.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import * as wasm from "irradiance-wasm";
-import * as wasm_bg from "irradiance-wasm/irradiance_wasm_bg.wasm";
 
 import {
   BufferGeometry,
-  Float32BufferAttribute,
   PerspectiveCamera,
   Points,
   PointsMaterial,
@@ -16,11 +14,8 @@ import {
 import * as consts from "../consts";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { range } from "fp-ts/NonEmptyArray";
-import { downloadBlob } from "../util";
-import { interval, Subject, zip } from "rxjs";
 
 const Wasm = () => {
-  const [rx] = useState(new Subject<Uint8Array>());
   useEffect(() => {
     const scene = new Scene();
     const camera = new PerspectiveCamera(
@@ -51,48 +46,10 @@ const Wasm = () => {
 
     camera.position.z = 5;
     requestAnimationFrame(() => renderer.render(scene, camera));
-    zip(interval(2000), rx).subscribe({
-      next([idx, buf]) {
-        downloadBlob(buf, idx.toString() + ".hdr");
-      },
-    });
   }, []);
-  const onclick = () => {
-    fetch("./venetian_crossroads_1k.hdr")
-      .then((x) => x.arrayBuffer())
-      .then((x) => new Uint8Array(x))
-      .then((ab) => {
-        wasm.irradiance(
-          10000,
-          64,
-          ab,
-          (idx: bigint, offset: number, size: bigint) => {
-            console.log(
-              idx,
-              offset,
-              size,
-              typeof idx,
-              typeof offset,
-              typeof size
-            );
-            const hdrBuf = new Uint8Array(
-              wasm_bg.memory.buffer,
-              offset,
-              Number(size)
-            );
-            const cp = new Uint8Array(new ArrayBuffer(Number(size)));
-            cp.set(hdrBuf);
-            rx.next(cp);
 
-            // console.log("aaa", x, y);
-          }
-        );
-      });
-    // .then((buf) => downloadBlob(buf, "a.hdr"));
-  };
   return (
     <div>
-      <button onClick={onclick}>click me</button>
       <div
         id={consts.threeDivId}
         style={{

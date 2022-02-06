87f7cafb65450304cd00491f2475f735d9733c1a
Ingun Jon <ingun37@gmail.com>
2022-02-06 15:20:39 +0900

    gh-pages

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  pathPrefix: "/irradiance-wasm",
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+};

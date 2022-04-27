8e78a7428c2a4e112d65a4c1581c456840345b64
Ingun Jon <ingun37@gmail.com>
2022-04-27 14:40:18 +0900

    three-debug.tsx

--- www/src/pages/three-debug.tsx
+++ www/src/pages/three-debug.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import { useEffect } from "react";
 export default function ThreeDebug(props: {
   domID?: string;
-  eff: (w: number, h: number, domID: string) => void;
+  eff: (w: number, h: number, domID: string) => any;
+  width?: number;
+  height?: number;
 }) {
   const domID =
     props.domID ?? "dom" + Math.floor(Math.random() * 100000).toString();
-  const width = 512;
-  const height = 512;
+  const width = props.width ?? 512;
+  const height = props.height ?? 512;
   useEffect(() => {
     props.eff(width, height, domID);
   }, []);
